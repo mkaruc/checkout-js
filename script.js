3afad7206a86f874a9d34579ac9e4c0ede289018
F.Mehmet Karuç <103605538+mkaruc@users.noreply.github.com>
2022-06-02 22:50:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const removeButton= document.getElementById("remove")
- removeButton.addEventListener("click", disableProduct())
-function disableProduct(){
-    document.getElementById("product").style.display= "none"
-}
